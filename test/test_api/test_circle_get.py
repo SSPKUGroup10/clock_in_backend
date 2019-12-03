85c2c1e7785b2adb9a95759e74ac94da89d7aa15
notfreshmbp <1195056983@qq.com>
2019-12-03 18:09:45 +0800

    update

--- test/test_api/test_circle_get.py
+++ test/test_api/test_circle_get.py
@@ -2,31 +2,33 @@ import requests as R
 
 from util import show_call
 
+host = "http://127.0.0.1:8000"
+api_url = "/api/v1"
 
 @show_call
 def test_circles_get():
-    ping_url = "http://127.0.0.1:8000/circles/"
+    ping_url = host + api_url + "/circles/"
     r = R.get(ping_url)
     print(r.status_code)
     print(r.json())
 
 @show_call
 def test_cirlcle_get_by_id():
-    ping_url = "http://127.0.0.1:8000/circles/1/"
+    ping_url =  host + api_url + "/circles/1/"
     r = R.get(ping_url)
     print(r.json())
 
 
 @show_call
 def test_cirlcle_get_by_id():
-    ping_url = "http://127.0.0.1:8000/circles/1/"
+    ping_url = host + api_url + "/circles/1/"
     r = R.get(ping_url)
     print(r.json())
 
 
 @show_call
 def test_cirlcle_update_by_id():
-    ping_url = "http://127.0.0.1:8000/circles/1/"
+    ping_url = host + api_url + "/circles/1/"
     data = {
         'name': '新名字2'
     }
@@ -35,7 +37,7 @@ def test_cirlcle_update_by_id():
 
 @show_call
 def test_cirlcle_delete_by_id():
-    ping_url = "http://127.0.0.1:8000/circles/1/"
+    ping_url = host + api_url + "/circles/1/"
     r = R.delete(ping_url)
     print(r.json())
 
